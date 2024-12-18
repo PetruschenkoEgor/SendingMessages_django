5946571a3b7306bb5017b2c7b4864ef03c318295
Egor Petruschenko <jaguar.94@mail.ru>
2024-12-18 16:09:25 +0600

    добавил админку

--- sending_messages/admin.py
+++ sending_messages/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
 
-# Register your models here.
+from sending_messages.models import Recipient, Message, Sender, Mailing, AttemptMailing
+
+
+@admin.register(Recipient)
+class RecipientAdmin(admin.ModelAdmin):
+    list_display = ('id', 'email', 'fio')
+    ordering = ('fio',)
+    search_fields = ('email', 'fio')
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'topic')
+
+
+@admin.register(Sender)
+class SenderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email')
+    ordering = ('name',)
+    search_fields = ('name', 'email')
+
+
+@admin.register(Mailing)
+class MailingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'sender', 'status')
+    list_filter = ('status',)
+
+
+@admin.register(AttemptMailing)
+class AttemptMailingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'date_time_attempt', 'status')
+    list_filter = ('status',)
