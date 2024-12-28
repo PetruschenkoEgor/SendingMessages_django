18ce46278f09cb9af773b30b0a989bcc3338d751
Egor Petruschenko <jaguar.94@mail.ru>
2024-12-28 18:58:47 +0600

    настроил выход

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 
 from django.urls import path
 
@@ -8,4 +8,5 @@ from users.forms import CustomAuthenticationForm
 app_name = UsersConfig.name
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='login.html', authentication_form=CustomAuthenticationForm), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
